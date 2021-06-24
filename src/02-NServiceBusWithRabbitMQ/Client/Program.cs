78bd53d68f956188f00f4ad12cfaeb7e6faa7409
Hadi Jahangiri <hadi.jahangiri2015@gmail.com>
2021-06-24 14:33:56 +0430

    Nservice bus with rabbit mq

---
+++ src/02-NServiceBusWithRabbitMQ/Client/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NServiceBus;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Client app";
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Waiting for receive message ...");
+            var configuration = new EndpointConfiguration("Client");
+             RabbitMqConfig(configuration);
+            var endpoint = Endpoint.Start(configuration).Result;
+
+            Console.ReadLine();
+        }
+
+        private static void RabbitMqConfig(EndpointConfiguration configuration)
+        {
+            configuration.SendFailedMessagesTo("Client.Errors");
+            configuration.EnableInstallers();
+            var transport = configuration.UseTransport<RabbitMQTransport>();
+            transport.UseConventionalRoutingTopology();
+            transport.ConnectionString("host=localhost");
+        }
+    }
+}
---
+++ src/02-NServiceBusWithRabbitMQ/Server/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Messages;
+using NServiceBus;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Server app";
+            var configuration = new EndpointConfiguration("Server");
+            RabbitMqConfig(configuration);
+             var endpoint = Endpoint.Start(configuration).Result;
+
+            while (true)
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.Write("Press enter to send message ...");
+                Console.ReadLine();
+                var userRegisterCommand = new UserRegisterCommand
+                {
+                    UserName = "User " + Guid.NewGuid(),
+                    CreateDate = DateTime.Now
+                };
+
+                endpoint.Send("Client", userRegisterCommand).Wait();
+            }
+        }
+
+        private static void RabbitMqConfig(EndpointConfiguration configuration)
+        {
+            configuration.SendFailedMessagesTo("Server.Errors");
+            configuration.EnableInstallers();
+            var transport = configuration.UseTransport<RabbitMQTransport>();
+            transport.UseConventionalRoutingTopology();
+            transport.ConnectionString("host=localhost");
+        }
+    }
+}
